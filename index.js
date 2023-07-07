3f6c9937e1ab7b15030a79e4c8e6779918d489c6
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-07 11:14:35 +0200

    feat: return Rock, Paper, Scissors randomly according to the random value

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function getComputerChoice(){
     let maxValue = 3;
     let minValue = 1;
-    let random = Math.floor(Math.random() *(maxValue - minValue + 1)) + minValue;
-    return random;
+    let randomValue = Math.floor(Math.random() *(maxValue - minValue + 1)) + minValue;
+    if (randomValue == 1){
+        return "Rock";
+    }
+    else if (randomValue == 2){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+   
 }
 
 console.log(getComputerChoice());
